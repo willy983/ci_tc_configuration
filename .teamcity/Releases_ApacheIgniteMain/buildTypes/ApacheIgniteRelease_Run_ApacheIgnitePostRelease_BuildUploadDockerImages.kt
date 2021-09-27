8ea211d7478d530fb339955969815f3673c90e54
vosipov <vosipov@TeamCity>
2021-09-27 12:08:53 +0300

    TeamCity change in '<Root project>' project: Cleanup old configuration files

--- .teamcity/Releases_ApacheIgniteMain/buildTypes/ApacheIgniteRelease_Run_ApacheIgnitePostRelease_BuildUploadDockerImages.kt
+++
@@ -1,7 +0,0 @@
-package Releases_ApacheIgniteMain.buildTypes
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-object ApacheIgniteRelease_Run_ApacheIgnitePostRelease_BuildUploadDockerImages : BuildType({
-    name = "-> Run [#2.1] :: Apache Ignite Post Release | Build & Upload Docker Images"
-})
