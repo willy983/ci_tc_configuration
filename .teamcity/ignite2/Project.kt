61e9aab9985c292c967e6b3f63646e39bfbd451c
Vitaliy Osipov <osipov.v.ale@sberbank.ru>
2021-12-30 02:20:28 +0300

    apply path project ignite2

---
+++ .teamcity/ignite2/Project.kt
@@ -0,0 +1,9 @@
+package ignite2
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+
+object Project : Project({
+    id("ignite2")
+    name = "[Apache Ignite 2.x]"
+})
