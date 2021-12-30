d54d8a660380e021ae9c5584fc4088761389d5a9
vosipov <vosipov@TeamCity>
2021-12-30 17:26:48 +0300

    TeamCity change in '[RELEASES] / Apache Ignite / Nightly' project: '[APACHE IGNITE NIGHTLY RELEASE] #3 :: Assemble Docker Image' build configuration settings were updated

--- .teamcity/patches/buildTypes/Releases_NightlyRelease_ApacheIgniteNightlyReleaseAssembleDockerImage.kts
+++ .teamcity/patches/buildTypes/Releases_NightlyRelease_ApacheIgniteNightlyReleaseAssembleDockerImage.kts
@@ -1,6 +1,7 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
@@ -44,6 +45,10 @@ changeBuildType(RelativeId("Releases_NightlyRelease_ApacheIgniteNightlyReleaseAs
     }
     steps {
         update<ScriptBuildStep>(0) {
+            enabled = true
+            clearConditions()
+        }
+        update<BuildStep>(1) {
             enabled = true
             clearConditions()
         }
