374cdbe92cfc72296e334e098baca39b9afaeaf5
vosipov <vosipov@TeamCity>
2022-04-01 15:54:37 +0300

    TeamCity change in 'Ignite Tests 2.x (JDK 8/11) / [DEV]' project: runners of 'test1' build configuration were updated

--- .teamcity/IgniteTests24Java8_Dev/buildTypes/IgniteTests24Java8_Dev_Test1.kt
+++ .teamcity/IgniteTests24Java8_Dev/buildTypes/IgniteTests24Java8_Dev_Test1.kt
@@ -1,8 +1,16 @@
 package IgniteTests24Java8_Dev.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 object IgniteTests24Java8_Dev_Test1 : BuildType({
     name = "test1"
     description = "test1"
+
+    steps {
+        script {
+            name = "show cpu"
+            scriptContent = "lscpu"
+        }
+    }
 })
