1d70919b6af1e6dd089ad3798493a8f9abef6885
vosipov <vosipov@TeamCity>
2022-05-24 00:47:57 +0300

    TeamCity change in 'TestAltLinIgnite Tests 2.x (JDK 8/11)' project: runners of '[Build]' build configuration were updated

--- .teamcity/TestAltLinIgniteTests2xJdk811/buildTypes/TestAltLinIgniteTests2xJdk811_BuildApacheIgnite.kt
+++ .teamcity/TestAltLinIgniteTests2xJdk811/buildTypes/TestAltLinIgniteTests2xJdk811_BuildApacheIgnite.kt
@@ -112,12 +112,15 @@ object TestAltLinIgniteTests2xJdk811_BuildApacheIgnite : BuildType({
                 REPOSITORY__DIR="%env.HOME%/.m2/repository/org/apache/ignite"
                 REPOSITORY__DIR__TEST="%env.HOME%/.m2/repository/com/sbt/ignite"
                 
+                if [ -d "${'$'}{REPOSITORY__DIR}" ]
+                then
+                    cp -rfv ${'$'}{REPOSITORY__DIR}/* repository/
+                fi
                 if [ -d "${'$'}{REPOSITORY__DIR__TEST}" ]
                 then
                 	REPOSITORY__DIR=${'$'}{REPOSITORY__DIR__TEST}
+                    cp -rfv ${'$'}{REPOSITORY__DIR}/* repository/
                 fi
-                
-                cp -rfv ${'$'}{REPOSITORY__DIR}/* repository/
             """.trimIndent()
         }
     }
