15a1e72c8e9365a0e17d3a7afa78cf3922608e96
admin1 <admin1@TeamCity>
2022-06-21 13:08:58 +0300

    TeamCity change in '[Apache Ignite: Extensions]' project: project settings were updated

--- .teamcity/IgniteExtensions/Project.kt
+++ .teamcity/IgniteExtensions/Project.kt
@@ -6,7 +6,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 object Project : Project({
     id("IgniteExtensions")
     name = "[Apache Ignite: Extensions]"
-    description = "test"
 
     subProject(IgniteExtensions_Tests.Project)
 })
