d1b6641ab7f30e17c11abcde4fe4012fb528d8fa
NSAmelchev <NSAmelchev@TeamCity>
2022-07-21 13:42:19 +0300

    TeamCity change in 'Ignite Tests 2.x (JDK 8/11)' project: parameters of 'Basic 2' build configuration were updated

--- .teamcity/IgniteTests24Java8/buildTypes/IgniteTests24Java8_Basic2.kt
+++ .teamcity/IgniteTests24Java8/buildTypes/IgniteTests24Java8_Basic2.kt
@@ -8,8 +8,8 @@ object IgniteTests24Java8_Basic2 : BuildType({
 
     params {
         text("JVM_ARGS", "-DIGNITE_MARSHAL_BUFFERS_RECHECK=1000", display = ParameterDisplay.HIDDEN, allowEmpty = true)
-        text("MAVEN_MODULES", ":ignite-schedule,:ignite-jcl,:ignite-log4j,:ignite-log4j2,:ignite-slf4j,:ignite-core", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("TEST_SUITE", "IgniteIpcTestSuite,IgniteSchedulerTestSuite,IgniteJclTestSuite,IgniteLog4jTestSuite,IgniteLog4j2TestSuite,IgniteSlf4jTestSuite,IgniteMessagingConfigVariationFullApiTestSuite,IgniteComputeBasicConfigVariationsFullApiTestSuite", display = ParameterDisplay.HIDDEN, allowEmpty = true)
+        text("MAVEN_MODULES", ":ignite-schedule,:ignite-jcl,:ignite-log4j2,:ignite-slf4j,:ignite-core", display = ParameterDisplay.HIDDEN, allowEmpty = true)
     }
 
     failureConditions {
