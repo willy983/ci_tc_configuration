e19aa207de1555dcb5fda65d140d41b8bf7940ae
NSAmelchev <NSAmelchev@TeamCity>
2022-07-21 13:43:48 +0300

    TeamCity change in 'Ignite Tests 2.x (JDK 8/11)' project: parameters of 'Service Grid' build configuration were updated

--- .teamcity/IgniteTests24Java8/buildTypes/IgniteTests24Java8_ServiceGrid.kt
+++ .teamcity/IgniteTests24Java8/buildTypes/IgniteTests24Java8_ServiceGrid.kt
@@ -7,7 +7,7 @@ object IgniteTests24Java8_ServiceGrid : BuildType({
     name = "Service Grid"
 
     params {
-        text("MAVEN_MODULES", ":ignite-schedule,:ignite-jcl,:ignite-log4j,:ignite-log4j2,:ignite-slf4j,:ignite-core", display = ParameterDisplay.HIDDEN, allowEmpty = true)
+        text("MAVEN_MODULES", ":ignite-schedule,:ignite-jcl,:ignite-log4j2,:ignite-slf4j,:ignite-core", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         param("MAVEN_PROFILES", "all-java,all-scala,scala,all-other,compatibility,lgpl,yardstick,benchmarks,examples,surefire-fork-count-1")
         text("JVM_ARGS", "-DIGNITE_MARSHAL_BUFFERS_RECHECK=1000", display = ParameterDisplay.HIDDEN, allowEmpty = true)
         text("TEST_SUITE", "IgniteServiceGridTestSuite,IgniteServiceConfigVariationsFullApiTestSuite", display = ParameterDisplay.HIDDEN, allowEmpty = true)
